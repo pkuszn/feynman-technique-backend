85d02eda8f1de10d91bc99466023eb5bbc9c3f79
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-02 12:27:09 +0200

    swagger in release

--- src/feynman-technique-backend/Program.cs
+++ src/feynman-technique-backend/Program.cs
@@ -14,11 +14,8 @@ builder.Services.AddSwaggerGen();
 
 WebApplication app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
