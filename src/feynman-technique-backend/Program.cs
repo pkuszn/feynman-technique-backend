2d5c0bf257d648acfbbd9707796a93cec99a6cc9
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-02 17:35:19 +0200

    run without rediretion

--- src/feynman-technique-backend/Program.cs
+++ src/feynman-technique-backend/Program.cs
@@ -17,17 +17,6 @@ WebApplication app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
 app.UseRouting();
 
 app.UseCors();
