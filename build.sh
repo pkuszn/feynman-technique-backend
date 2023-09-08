0d6684cb8d3d1e9fe5ad74bf67f77a7c38b76b26
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-08 22:17:19 +0200

    missing then

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ dos2unix ./db/init.sql
 
 echo "> Copying scripts..."
 sudo docker cp ./db/init.sql $name:/root/init.sql
-if [[ $1 == *-c*]]
+if [[ $1 == *-c*]]; then
 	docker exec $name /bin/sh -c 'mysql -u root -proot ft-db </root/init.sql'
