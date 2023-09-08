191baedc16dfc2dd1d8d43f504f160e387f92bf5
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-08 22:22:40 +0200

    update script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 name="ft-db"
 
 docker compose up -d
@@ -6,6 +8,6 @@ dos2unix ./db/init.sql
 
 echo "> Copying scripts..."
 sudo docker cp ./db/init.sql $name:/root/init.sql
-if [[ $1 == *-c*]]; then
+if echo "$@" | grep -e "-c" -q; then
 	docker exec $name /bin/sh -c 'mysql -u root -proot ft-db </root/init.sql'
 fi
