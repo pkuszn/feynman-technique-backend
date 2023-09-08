87afd3a6c748366063fa8f8d3715679e1b039ebd
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-08 22:06:09 +0200

    port update & created rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,1 @@
+docker rm -f feynman-technique-backend && docker rmi feynman-technique-backend-feynman-technique-backend:latest && git reset --hard && git.exe pull && dos2unix build.sh && chmod +x build.sh && ./build.sh
