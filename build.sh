3204679077f2577016e70c2e520488b519f6c247
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-08 22:18:16 +0200

    missing fi

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ echo "> Copying scripts..."
 sudo docker cp ./db/init.sql $name:/root/init.sql
 if [[ $1 == *-c*]]; then
 	docker exec $name /bin/sh -c 'mysql -u root -proot ft-db </root/init.sql'
+fi
