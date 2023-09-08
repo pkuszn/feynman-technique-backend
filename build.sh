ed7181db6c1c994585c850391f5288a092735106
patryk <patryk.kuszneruk1798@gmail.com>
2023-09-08 22:14:58 +0200

    db init flag

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ dos2unix ./db/init.sql
 
 echo "> Copying scripts..."
 sudo docker cp ./db/init.sql $name:/root/init.sql
-docker exec $name /bin/sh -c 'mysql -u root -proot ft-db </root/init.sql'
+if [[ $1 == *-c*]]
+	docker exec $name /bin/sh -c 'mysql -u root -proot ft-db </root/init.sql'
